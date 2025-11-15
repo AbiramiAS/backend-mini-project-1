ba9f3308729fed9d6abbff5d46e17cdd1f05362c
AbiramiAS <abiramishanmugamvkl@gmail.com>
2025-11-15 12:46:07 +0530

    CRUD in API

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 import Express from "express";
 import path from "path";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+import cors from "cors";
+// import employeeData from "./model/employees.json";
+import corsOptions from "./configs/corsOptions.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
 
-dotenv.config();
+// dotenv.config();
 const __dirname = path.resolve();
 
 const app = Express();
+
+app.use(Express.json());
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+
+//route middleware
+app.use("/employees", employeeRoutes); 
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+
+//port configuration
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
